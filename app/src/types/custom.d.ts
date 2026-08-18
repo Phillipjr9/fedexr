f0699d3ae6574f19e0ba101f630749e1170de57c
BTC <progressive@BTC-MacBook-Pro.local>
2026-08-18 14:07:41 -0400

    chore(types): declare react-i18next and i18next modules for TS

--- app/src/types/custom.d.ts
+++ app/src/types/custom.d.ts
@@ -1,6 +1,18 @@
 declare module 'i18n-iso-countries';
 declare module '*.json';
 
+declare module 'react-i18next' {
+  import * as React from 'react';
+  export function useTranslation(ns?: string | string[]): { t: (k: string, opts?: any) => string; i18n: any };
+  export const I18nextProvider: React.ComponentType<any>;
+  export const Trans: React.ComponentType<any>;
+}
+
+declare module 'i18next' {
+  const i18n: any;
+  export default i18n;
+}
+
 interface Window {
   i18n?: any;
 }
